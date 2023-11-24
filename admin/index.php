008ef4ea7eeddd714f04ab285f69922de7b0497b
fahradfoysal <edu.farhad.foysal@gmail.com>
2023-11-25 05:29:10 +0600

    m

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,13 @@ include('../db/db.php');
 if(isset($_SESSION['user'])){
     $_SESSION['visitor_id']=$_SESSION['user'];
     $_SESSION['unique_id']=$_SESSION['user'];
-    $_SESSION['role']='member';
+    $_SESSION['role']='admin';
+    if($_SESSION['b_id']==''){
+        header("location:create");
+    }else{
+        $b_id = $_SESSION['b_id'];
+    }
+
 }else{
     header("location:login");
 }
