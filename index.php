e07cb3e0096c5ea91604dee5a753cca592f8a71f
fahradfoysal <edu.farhad.foysal@gmail.com>
2023-11-26 11:10:15 +0600

    products

--- index.php
+++ index.php
@@ -803,7 +803,7 @@
         </div>
     </section>
 
-    <section style="margin: 0 auto" class="product_category text-center " id="product_category">
+    <section style="margin: 0 auto" class="product_category" id="product_category">
         <div class="heading">
             <h1>Products Categories </h1>
             <a href="#" class="btn">Choose<i class='bx bx-right-arrow-alt'></i></a>
@@ -838,11 +838,92 @@
          
 
          </nav>
-        <div id="products" class="px-0 m-auto">
+        <div id="products" class="products px-0 m-auto">
         <div class="heading m-auto">
             <h1>Products Details </h1>
             <a href="#" class="btn">Add Cart<i class='bx bx-right-arrow-alt'></i></a>
         </div> 
+
+
+
+        <div class="container-fluid">
+
+
+      
+        <div class="bg-gray-100 w-full min-h-screen gap-4 flex-wrap flex justify-center items-center">
+
+  <!-- Card -->
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+    <!-- Image -->
+    <img class="h-40 object-cover rounded-xl" h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
+    <div class="p-2">
+    <!-- Heading -->
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+    <!-- Description -->
+    <p class="text-sm text-gray-600">Simple Yet Beautiful Card Design with TaiwlindCss. Subscribe to our Youtube channel for more ...</p>
+    </div>
+    <!-- CTA -->
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-purple-600 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+    <!-- Card -->
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+    <!-- Image -->
+    <img class="h-40 object-cover rounded-xl" h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
+    <div class="p-2">
+    <!-- Heading -->
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+    <!-- Description -->
+    <p class="text-sm text-gray-600">Simple Yet Beautiful Card Design with TaiwlindCss. Subscribe to our Youtube channel for more ...</p>
+    </div>
+    <!-- CTA -->
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-sky-500 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+  
+    <!-- Card -->
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+    <!-- Image -->
+    <img class="h-40 object-cover rounded-xl" h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
+    <div class="p-2">
+    <!-- Heading -->
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+    <!-- Description -->
+    <p class="text-sm text-gray-600">Simple Yet Beautiful Card Design with TaiwlindCss. Subscribe to our Youtube channel for more ...</p>
+    </div>
+    <!-- CTA -->
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-green-500 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+      <!-- Card -->
+  <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+    <!-- Image -->
+    <img class="h-40 object-cover rounded-xl" h-40 object-cover rounded-xl" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
+    <div class="p-2">
+    <!-- Heading -->
+    <h2 class="font-bold text-lg mb-2 ">Heading</h2>
+    <!-- Description -->
+    <p class="text-sm text-gray-600">Simple Yet Beautiful Card Design with TaiwlindCss. Subscribe to our Youtube channel for more ...</p>
+    </div>
+    <!-- CTA -->
+    <div class="m-2">
+    <a role='button' href='#' class="text-white bg-yellow-500 px-3 py-1 rounded-md hover:bg-purple-700">Learn More</a>
+    </div>
+  </div>
+  
+  
+  
+
+</div>
+
+
+
+        </div>
+
+
 
         </div>
 
