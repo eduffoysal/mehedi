2de2bd810dfbf97caf933c5d0f85c4da61b37106
fahradfoysal <edu.farhad.foysal@gmail.com>
2023-12-20 12:17:30 +0600

    add index to remove design or product from cart

--- product/design_product.php
+++ product/design_product.php
@@ -412,10 +412,10 @@ if(isset($_POST['order_cart'])){
 
                 $orderList = convertSessionToObjects('product_cart');
 
-                foreach ($orderList as $list) {
+                foreach ($orderList as $index => $list) {
                     ?>
 
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" id="<?=$list->getProId()?>">
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" id="<?=$index?>">
                             <td class="p-4">
                                 <img src="<?=$list->getProImage()?>" class="w-16 md:w-32 max-w-full max-h-full" alt="<?=$list->getProName()?>">
                             </td>
@@ -424,16 +424,16 @@ if(isset($_POST['order_cart'])){
                             </td>
                             <td class="px-6 py-4">
                                 <div class="flex items-center">
-                                    <button class="inline-flex minus_btn items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="m_<?=$list->getProId()?>" type="button">
+                                    <button class="inline-flex minus_btn items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="m_<?=$index?>" type="button">
                                         <span class="sr-only">Quantity button</span>
                                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
                                         </svg>
                                     </button>
                                     <div>
-                                        <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="product_q_<?=$list->getProId()?>" value="<?=$list->getQuantity()?>" placeholder="1" required>
+                                        <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="product_q_<?=$index?>" value="<?=$list->getQuantity()?>" placeholder="1" required>
                                     </div>
-                                    <button class="inline-flex plus_btn items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="p_<?=$list->getProId()?>" type="button">
+                                    <button class="inline-flex plus_btn items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="p_<?=$index?>" type="button">
                                         <span class="sr-only">Quantity button</span>
                                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -445,7 +445,7 @@ if(isset($_POST['order_cart'])){
                             <?=$list->getPrice()?>
                             </td>
                             <td class="px-6 py-4">
-                                <a href="#" class="font-medium remove_product_btn text-red-600 dark:text-red-500 hover:underline" id="<?=$list->getProId()?>">Remove</a>
+                                <a href="#" class="font-medium remove_product_btn text-red-600 dark:text-red-500 hover:underline" id="<?=$index?>">Remove</a>
                             </td>
                         </tr>
 
@@ -476,10 +476,10 @@ if(isset($_POST['booking_cart'])){
 
         $orderList = convertSessionToObjects('design_cart');
 
-        foreach ($orderList as $list) {
+        foreach ($orderList as $index => $list) {
             ?>
 
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" id="<?=$list->getProId()?>">
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" id="<?=$index?>">
                     <td class="p-4">
                         <img src="<?=$list->getProImage()?>" class="w-16 md:w-32 max-w-full max-h-full" alt="<?=$list->getProName()?>">
                     </td>
@@ -488,16 +488,16 @@ if(isset($_POST['booking_cart'])){
                     </td>
                     <td class="px-6 py-4">
                         <div class="flex items-center">
-                            <button class="inline-flex minus_btn items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="m_<?=$list->getProId()?>" type="button">
+                            <button class="inline-flex minus_btn items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="m_<?=$index?>" type="button">
                                 <span class="sr-only">Quantity button</span>
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
                                 </svg>
                             </button>
                             <div>
-                                <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="<?=$list->getQuantity()?>" placeholder="1" id="design_q_<?=$list->getProId()?>" required>
+                                <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="<?=$list->getQuantity()?>" placeholder="1" id="design_q_<?=$index?>" required>
                             </div>
-                            <button class="inline-flex plus_btn items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="p_<?=$list->getProId()?>" type="button">
+                            <button class="inline-flex plus_btn items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" id="p_<?=$index?>" type="button">
                                 <span class="sr-only">Quantity button</span>
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -509,7 +509,7 @@ if(isset($_POST['booking_cart'])){
                     <?=$list->getPrice()?>
                     </td>
                     <td class="px-6 py-4">
-                        <a href="#" class="font-medium remove_design_btn text-red-600 dark:text-red-500 hover:underline" id="<?=$list->getProId()?>">Remove</a>
+                        <a href="#" class="font-medium remove_design_btn text-red-600 dark:text-red-500 hover:underline" id="<?=$index?>">Remove</a>
                     </td>
                 </tr>
 
